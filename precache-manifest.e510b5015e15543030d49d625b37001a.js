6f275fbff514b85ab9437abd842b9d8925c9dc9f
lukastatarunas <lukasttatarunas@gmail.com>
2019-08-21 19:39:35 +0300

    Updates

---
+++ precache-manifest.e510b5015e15543030d49d625b37001a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab37197e25b899dcf1edd2fb3a06ddb",
+    "url": "/Portfolio-Web-App/index.html"
+  },
+  {
+    "revision": "618564507fc859998be8",
+    "url": "/Portfolio-Web-App/static/css/2.3b3361de.chunk.css"
+  },
+  {
+    "revision": "36adc56eaa3f6b402f0f",
+    "url": "/Portfolio-Web-App/static/css/main.f479ce2b.chunk.css"
+  },
+  {
+    "revision": "618564507fc859998be8",
+    "url": "/Portfolio-Web-App/static/js/2.8cba323e.chunk.js"
+  },
+  {
+    "revision": "36adc56eaa3f6b402f0f",
+    "url": "/Portfolio-Web-App/static/js/main.4362f394.chunk.js"
+  },
+  {
+    "revision": "b791030bb39075fdbb51",
+    "url": "/Portfolio-Web-App/static/js/runtime~main.5c9bfb22.js"
+  },
+  {
+    "revision": "0cc3212267572d1c23af25d3277f524f",
+    "url": "/Portfolio-Web-App/static/media/CV.0cc32122.pdf"
+  },
+  {
+    "revision": "112dc134575a6b8fa4a72355ba7d6c1a",
+    "url": "/Portfolio-Web-App/static/media/logo-green.112dc134.svg"
+  },
+  {
+    "revision": "87b86d7b168dcd56bb1aadb0f0c2934a",
+    "url": "/Portfolio-Web-App/static/media/logo-white.87b86d7b.svg"
+  },
+  {
+    "revision": "f694d73f498df7ce59d07016a948a26e",
+    "url": "/Portfolio-Web-App/static/media/robofriends-logo.f694d73f.svg"
+  },
+  {
+    "revision": "b2e3a275f02938e9bdd7214297dee19d",
+    "url": "/Portfolio-Web-App/static/media/robofriends.b2e3a275.png"
+  },
+  {
+    "revision": "ce7609db4d4c1915973bb77b8636e5b1",
+    "url": "/Portfolio-Web-App/static/media/smartbrain-logo.ce7609db.svg"
+  },
+  {
+    "revision": "67b05d8c879331f63d75810d230e1bdd",
+    "url": "/Portfolio-Web-App/static/media/smartbrain.67b05d8c.png"
+  }
+]);
