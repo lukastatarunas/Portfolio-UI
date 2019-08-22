5e372a0e30fecb5fd1c6b6d1d91f23c4575dfc14
lukastatarunas <lukasttatarunas@gmail.com>
2019-08-22 14:17:47 +0300

    Added more projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,9 @@ import smartBrain from './assets/smartbrain.png'
 import smartBrainLogo from './assets/smartbrain-logo.svg'
 import roboFriends from './assets/robofriends.png'
 import roboFriendsLogo from './assets/robofriends-logo.svg'
+import lrt from './assets/lrt.jpg'
+import calculator from './assets/calculator.png'
+import calculatorLogo from './assets/calculator-logo.svg'
 import './Projects.css'
 
 const Projects = () => {
@@ -47,14 +50,14 @@ const Projects = () => {
               </div>
             </div>
             <p className='description'>
-              Built using React.js and Machine Learning Clarifai API.
+              Built using React.js, Node.js, Express.js, Knex.js, PostgreSQL and Machine Learning Clarifai API.
             </p>
           </figcaption>
         </figure>
         <figure className='effect'>
           <div className='project-logo-container'>
             <img
-              className='projects-logo smart-logo'
+              className='projects-logo robo-logo'
               src={ roboFriendsLogo }
               alt='project'
             />
@@ -89,6 +92,88 @@ const Projects = () => {
             </div>
             <p className='description'>
               Built using React.js and Redux.js.
+            </p>
+          </figcaption>
+        </figure>
+        <figure className='effect'>
+          {/* <div className='project-logo-container'>
+            <img
+              className='projects-logo smart-logo'
+              src={ smartBrainLogo }
+              alt='project'
+            />
+          </div> */}
+          <div className='background-container'>
+            <img
+              className='image-background'
+              src={ lrt }
+              alt='project'
+            />
+          </div>
+          <figcaption>
+            <div className='bottom-container'>
+              <div className='portfolio-links'>
+                <a
+                  className='project-button'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://github.com/lukastatarunas/UI'
+                >
+                  Github
+                </a>
+                <a
+                  className='project-button'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://lrt-live.herokuapp.com'
+                >
+                  Website
+                </a>
+              </div>
+            </div>
+            <p className='description'>
+              Built using React.js, Node.js, Express.js and LRT API.
+            </p>
+          </figcaption>
+        </figure>
+        <figure className='effect'>
+          <div className='project-logo-container'>
+            <img
+              className='projects-logo calculator-logo'
+              src={ calculatorLogo }
+              alt='project'
+            />
+          </div>
+          <div className='background-container'>
+            <img
+              className='image-background'
+              src={ calculator }
+              alt='project'
+            />
+          </div>
+          <figcaption>
+            <div className='bottom-container'>
+              <div className='portfolio-links'>
+                <a
+                  className='project-button'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://github.com/lukastatarunas/iPhone-Calculator-Web-App'
+                >
+                  Github
+                </a>
+                <a
+                  className='project-button'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://lukastatarunas.github.io/iPhone-Calculator-Web-App/'
+                >
+                  Website
+                </a>
+              </div>
+            </div>
+            <p className='description'>
+              Built using React.js.
             </p>
           </figcaption>
         </figure>
