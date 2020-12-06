fff23dac66e938bba1649b9aeeaa02915352965c
lukas.tatarunas@shawk.eu <lukas.tatarunas@shawk.eu>
2020-12-06 19:28:37 +0200

    add projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import campaigns from './assets/campaigns.png'
+import eCommerce from './assets/ecommerce.png'
 import smartBrain from './assets/smartbrain.png'
 import smartBrainLogo from './assets/smartbrain-logo.svg'
 import roboFriends from './assets/robofriends.png'
@@ -7,10 +9,10 @@ import lrt from './assets/lrt.jpg'
 import memoryGame from './assets/game.jpg'
 import gradient from './assets/gradient.png'
 import gradientLogo from './assets/gradient-logo.svg'
-import calculator from './assets/calculator.jpg'
-import calculatorLogo from './assets/calculator-logo.svg'
+// import calculator from './assets/calculator.jpg'
+// import calculatorLogo from './assets/calculator-logo.svg'
 import camperbnb from './assets/camperbnb.jpg'
-import troll from './assets/troll.jpg'
+// import troll from './assets/troll.jpg'
 import './Projects.css'
 
 const Projects = () => {
@@ -18,6 +20,88 @@ const Projects = () => {
     <section className='projects'>
       <h1 className='section-header'>Projects</h1>
       <div className='grid'>
+        <figure className='effect'>
+          {/* <div className='project-logo-container'>
+            <img
+              className='projects-logo smart-logo'
+              src={ smartBrainLogo }
+              alt='project'
+            />
+          </div> */}
+          <div className='background-container'>
+            <img
+              className='image-background'
+              src={ eCommerce }
+              alt='project'
+            />
+          </div>
+          <figcaption>
+            <div className='bottom-container'>
+              <div className='portfolio-links'>
+                <a
+                  className='project-button'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://github.com/lukastatarunas/E-Commerce-Web-App'
+                >
+                  Github
+                </a>
+                <a
+                  className='project-button'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://lukas-crown-clothing.herokuapp.com/'
+                >
+                  Website
+                </a>
+              </div>
+            </div>
+            <p className='description'>
+              Built using React.js, Redux.js, GraphQL, Context API, Stripe API, Firebase, styled-components.
+            </p>
+          </figcaption>
+        </figure>
+        <figure className='effect'>
+          {/* <div className='project-logo-container'>
+            <img
+              className='projects-logo smart-logo'
+              src={ smartBrainLogo }
+              alt='project'
+            />
+          </div> */}
+          <div className='background-container'>
+            <img
+              className='image-background'
+              src={ campaigns }
+              alt='project'
+            />
+          </div>
+          <figcaption>
+            <div className='bottom-container'>
+              <div className='portfolio-links'>
+                <a
+                  className='project-button'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://github.com/lukastatarunas/Campaigns-Web-App'
+                >
+                  Github
+                </a>
+                <a
+                  className='project-button'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://friendly-stonebraker-874097.netlify.app/'
+                >
+                  Website
+                </a>
+              </div>
+            </div>
+            <p className='description'>
+              Built using React.js, Redux.js, Moment.js, Material UI, styled-components.
+            </p>
+          </figcaption>
+        </figure>
         <figure className='effect'>
           <div className='project-logo-container'>
             <img
@@ -144,6 +228,47 @@ const Projects = () => {
         <figure className='effect'>
           {/* <div className='project-logo-container'>
             <img
+              className='projects-logo smart-logo'
+              src={ smartBrainLogo }
+              alt='project'
+            />
+          </div> */}
+          <div className='background-container'>
+            <img
+              className='image-background'
+              src={ camperbnb }
+              alt='project'
+            />
+          </div>
+          <figcaption>
+            <div className='bottom-container'>
+              <div className='portfolio-links'>
+                <a
+                  className='project-button'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://github.com/lukastatarunas/Camperbnb/'
+                >
+                  Github
+                </a>
+                <a
+                  className='project-button'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://camper-bnb.herokuapp.com/'
+                >
+                  Website
+                </a>
+              </div>
+            </div>
+            <p className='description'>
+              Built using React.js.
+            </p>
+          </figcaption>
+        </figure>
+        <figure className='effect'>
+          {/* <div className='project-logo-container'>
+            <img
               className='projects-logo memory-logo'
               src={ smartBrainLogo }
               alt='project'
@@ -223,7 +348,7 @@ const Projects = () => {
             </p>
           </figcaption>
         </figure>
-        <figure className='effect'>
+        {/* <figure className='effect'>
           <div className='project-logo-container'>
             <img
               className='projects-logo calculator-logo'
@@ -263,56 +388,15 @@ const Projects = () => {
               Built using React.js.
             </p>
           </figcaption>
-        </figure>
-        <figure className='effect'>
-          {/* <div className='project-logo-container'>
+        </figure> */}
+        {/* <figure className='effect'>
+          <div className='project-logo-container'>
             <img
               className='projects-logo smart-logo'
               src={ smartBrainLogo }
-              alt='project'
-            />
-          </div> */}
-          <div className='background-container'>
-            <img
-              className='image-background'
-              src={ camperbnb }
               alt='project'
             />
           </div>
-          <figcaption>
-            <div className='bottom-container'>
-              <div className='portfolio-links'>
-                <a
-                  className='project-button'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  href='https://github.com/lukastatarunas/Camperbnb/'
-                >
-                  Github
-                </a>
-                <a
-                  className='project-button'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  href='https://camper-bnb.herokuapp.com/'
-                >
-                  Website
-                </a>
-              </div>
-            </div>
-            <p className='description'>
-              Built using React.js.
-            </p>
-          </figcaption>
-        </figure>
-        <figure className='effect'>
-          {/* <div className='project-logo-container'>
-            <img
-              className='projects-logo smart-logo'
-              src={ smartBrainLogo }
-              alt='project'
-            />
-          </div> */}
           <div className='background-container'>
             <img
               className='image-background'
@@ -345,7 +429,7 @@ const Projects = () => {
               Built using React.js.
             </p>
           </figcaption>
-        </figure>
+        </figure> */}
       </div>
     </section>
   )
